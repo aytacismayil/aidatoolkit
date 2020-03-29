fa5eb9616358f7f1811127f2e82f71241b97e417
aytacismayil <aytac.smayil.97@mail.ru>
2020-03-29 17:30:57 +0400

    aidatoolkit

--- style/mystyle/js/style.js
+++ style/mystyle/js/style.js
@@ -1,7 +1,12 @@
-$(".click-open-close").click(function () {
-    $(this).toggleClass("fas-change");
-});
-
 $('.popover-dismiss').popover({
     trigger: 'hover'
 });
+
+$(document).ready(function(){
+    $('.btn-menu').on('click', function(){
+      $(this).toggleClass('btn-menu_active');
+      $('.popap').toggleClass('popap_active');
+      $('.popap-bg').toggleClass('popap-bg_active');
+    });
+    
+  });
