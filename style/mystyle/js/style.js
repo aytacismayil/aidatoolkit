cc14f7a2a0ed3253fb39e3a915156473c6233491
aytacismayil <aytac.smayil.97@mail.ru>
2020-04-06 13:27:26 +0400

    remove-add

--- style/mystyle/js/style.js
+++ style/mystyle/js/style.js
@@ -29,3 +29,38 @@ $(document).ready(function(){
     }
   }
 
+
+
+  $(".flow-plus").click(function () {
+    $(".vv").append('<input class="create-project-input form-control pt-1" placeholder="Start typing an employee s last name or nickname"></input> ');
+ });
+
+ var elet_create=document.querySelector('.flow-butoon')
+ $(".flow-pluss").click(function () {
+  $(".flow-bu").append('<button  class="flow-butoon"> Design <i class="fas fa-times"><i/></button>');
+}); 
+
+$(document).ready(function(){
+    $(".fa-times").click(function(){
+      $(this).parent('.flow-butoon').remove();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".edit-button-img").click(function(){
+      $(this).parent('.edit-curent-div-file').parent().remove();
+    });
+  });
+
+
+  var elem=document.querySelector('.edit-file-begin');
+
+  $(".edit-uppload").click(function () {
+    $(".edit-files").append(elem);
+ });
+
+ $(document).ready(function(){
+  $(".delete-edit-user").click(function(){
+    $(this).parent('.edit-user-delete').remove();
+  });
+});
